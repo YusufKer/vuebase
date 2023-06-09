c50617b03c9178680b6e7b32b61a2dbf08e9d87a
= <yus.ker1994@gmail.com>
2023-06-09 15:17:45 +0200

    sorted post by date

--- src/components/PostListing.vue
+++ src/components/PostListing.vue
@@ -1,8 +1,7 @@
 <template>
     <div class="p-4 bg-yellow-50 grid gap-4">
         <button @click.prevent="() => store.dispatch('getUsersPosts')" class="bg-red-50 py-2 px-6 rounded-xl">Refresh</button>
-        <Post v-for="(post,index) in store.state.posts" :key="index" :post-data="post"/>
-        {{ store.state.posts }}
+        <Post v-for="(post,index) in formatedPosts" :key="index" :post-data="post"/>
     </div>
 </template>
 
@@ -11,10 +10,14 @@
         TODO:
         [ ] build ui for posts
     */ 
-    import { onMounted } from 'vue';
+    import { onMounted, computed } from 'vue';
     import { useStore } from 'vuex';
 
     const store = useStore();
+    const formatedPosts = computed(()=>{
+        const formattedByDate = store.state.posts.sort((a,b) => a.date.seconds < b.date.seconds);
+        return formattedByDate
+    })
 
     onMounted(()=>{
         if(store.state.posts.length > 0) return
